90fd1c1d4b0515d23dc35eca62b0f562f6c23532
Sujeet0101 <suj337raut@gmail.com>
2025-05-22 16:51:52 +0545

    initial commit

---
+++ my-react-app/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+   
+    return (
+      <header >
+        <div className="logo" >LOGO</div>
+        <input type="text" />
+      </header>
+    );
+  }
+}
+
+export default Header;
